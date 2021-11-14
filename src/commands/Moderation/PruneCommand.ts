cc6e97e4ad4c5d1bceccee611faefeb48b2dcf26
KChi <59391215+KagChi@users.noreply.github.com>
2021-11-14 16:36:47 +0700

    feat(PruneCommand): add prune command

---
+++ src/commands/Moderation/PruneCommand.ts
@@ -0,0 +1,52 @@
+import { CommandOptions, Command, Args } from "@sapphire/framework";
+import { ApplyOptions } from "@sapphire/decorators";
+import { Collection, Message, MessageEmbed } from "discord.js";
+import { botEmbedColor } from "../../config";
+
+@ApplyOptions<CommandOptions>({
+    name: "prune",
+    description: "prune user message or channel message",
+    requiredClientPermissions: ["SEND_MESSAGES"]
+})
+
+export class ClientCommand extends Command {
+    async messageRun(message: Message, args: Args) {
+        const lookupUserArgs = await args.pickResult("member");
+        /* eslint no-negated-condition: "off" */
+        if (!lookupUserArgs.success) {
+            const messageToDeleteArgs = await args.pickResult("number");
+            if (message.channel.isText() && message.inGuild()) {
+                const deletedMessage = await message.channel.bulkDelete(messageToDeleteArgs.value ?? 10, true);
+                message.channel.send({
+                    embeds: [
+                        new MessageEmbed()
+                            .setDescription(`✅ | Pruned ${deletedMessage.size} messages`)
+                            .setColor(botEmbedColor)
+                    ]
+                });
+            }
+        } else {
+            const messageToDeleteArgs = await args.pickResult("number");
+            const filteredMessage = this.transformToCollection([...message.channel.messages.cache.filter(x => x.author.id === lookupUserArgs.value.user.id).values()].splice(0, messageToDeleteArgs.value ?? 10));
+            if (message.channel.isText() && message.inGuild()) {
+                const deletedMessage = await message.channel.bulkDelete(filteredMessage, true);
+                message.channel.send({
+                    embeds: [
+                        new MessageEmbed()
+                            .setDescription(`✅ | Pruned ${deletedMessage.size} ${lookupUserArgs.value.displayName} messages`)
+                            .setColor(botEmbedColor)
+                    ]
+                });
+            }
+        }
+    }
+
+    public transformToCollection(array: Message<boolean>[]) {
+        const temporaryCollection: Collection<string, Message<boolean>> = new Collection();
+        for (const data of array) {
+            temporaryCollection.set(data.id, data);
+            continue;
+        }
+        return temporaryCollection;
+    }
+}
