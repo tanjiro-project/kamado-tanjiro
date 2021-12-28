5de6537ac70ef9bb22891c8cba2f7bd4b2c31fac
KChi <59391215+KagChi@users.noreply.github.com>
2021-12-28 09:10:53 +0700

    feat(TanjiroClient): remove task on complete and fail

--- src/structures/TanjiroClient.ts
+++ src/structures/TanjiroClient.ts
@@ -31,6 +31,10 @@ export class TanjiroClient extends SapphireClient {
                             username: redisUsername,
                             password: redisPassword,
                             port: redisPort
+                        },
+                        defaultJobOptions: {
+                            removeOnComplete: true,
+                            removeOnFail: true
                         }
                     }
                 })
